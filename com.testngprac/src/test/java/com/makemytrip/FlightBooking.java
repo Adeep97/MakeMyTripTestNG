aeeaf1824d294e03506ac4f6f085fbec9a0bfa8a
adeedv <adeep.dv@capgemini.com>
2024-04-30 18:39:15 +0530

    adding methods in flightbooking class

--- com.testngprac/src/test/java/com/makemytrip/FlightBooking.java
+++ com.testngprac/src/test/java/com/makemytrip/FlightBooking.java
@@ -4,11 +4,15 @@ import java.time.Duration;
 
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 import com.utilites.BaseClass;
@@ -22,6 +26,13 @@ public class FlightBooking extends BaseClass{
 	By loginCloseBtn= By.xpath("//div[@class='imageSlideContainer']//span[@class='commonModal__close']");
 	By flightTab= By.xpath("//ul[@class='makeFlex font12 headerIconsGap']//span[text()='Flights']//ancestor::li[@class='menu_Flights']");
 	By oneWay= By.xpath("//div[contains(@class, 'makeFlex')]//li[text()='One Way']");
+	By fromCity =By.xpath("//input[@id='fromCity']/../..");
+	
+//	For debugging
+	@AfterMethod
+	public void waitTime() throws InterruptedException {
+		Thread.sleep(3000);
+	}
 	
 	
 	@Test(priority=1)
@@ -58,5 +69,15 @@ public class FlightBooking extends BaseClass{
 		WebElement one_way=wait.until(ExpectedConditions.visibilityOfElementLocated(oneWay));
 		one_way.click();
 	}
+	
+	@Test(priority=5)
+	public void fromCity() {
+		WebElement from_city=driver.findElement(fromCity);
+		from_city.click();
+		driver.findElement(By.xpath("//input[@class='react-autosuggest__input react-autosuggest__input--open']")).sendKeys("Bengaluru");
+		driver.findElement(By.xpath("//span[text()='Bengaluru']//ancestor::li")).click();
+	}
+	
+	
 
 }
