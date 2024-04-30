ed6bd0782dd100030a5fcab3b6d156ccbf194bab
adeedv <adeep.dv@capgemini.com>
2024-04-30 17:12:30 +0530

    adding flight booking tests

--- com.testngprac/src/test/java/com/makemytrip/FlightBooking.java
+++ com.testngprac/src/test/java/com/makemytrip/FlightBooking.java
@@ -5,6 +5,8 @@ import java.time.Duration;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
@@ -12,6 +14,15 @@ import org.testng.annotations.Test;
 import com.utilites.BaseClass;
 
 public class FlightBooking extends BaseClass{
+	WebDriverWait wait;
+	
+	//Locators
+	By noKiddingSalePopup=By.id("webklipper-publisher-widget-container-notification-frame");
+	By noKiddingSaleCloseBtn = By.xpath("//div[@id='webklipper-publisher-widget-container-notification-container']//a[@id='webklipper-publisher-widget-container-notification-close-div']");
+	By loginCloseBtn= By.xpath("//div[@class='imageSlideContainer']//span[@class='commonModal__close']");
+	By flightTab= By.xpath("//ul[@class='makeFlex font12 headerIconsGap']//span[text()='Flights']//ancestor::li[@class='menu_Flights']");
+	By oneWay= By.xpath("//div[contains(@class, 'makeFlex')]//li[text()='One Way']");
+	
 	
 	@Test(priority=1)
 	public void urlNavigation() {
@@ -24,14 +35,28 @@ public class FlightBooking extends BaseClass{
 	}
 	
 	@Test(priority=2)
-	public void removeCookies() throws InterruptedException {
-//		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-//		WebElement e1=wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='webklipper-publisher-widget-container-notification-container']//a[@id='webklipper-publisher-widget-container-notification-close-div']")));
-//		e1.click();
+	public void removePopups(){
+		wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+		WebElement nokiddingSale_popup=wait.until(ExpectedConditions.visibilityOfElementLocated(noKiddingSalePopup));
+		driver.switchTo().frame(nokiddingSale_popup);
+		WebElement noKidding_closebtn=wait.until(ExpectedConditions.visibilityOfElementLocated(noKiddingSaleCloseBtn));
+		noKidding_closebtn.click();
+		driver.switchTo().defaultContent();
+		WebElement login_closebtn=wait.until(ExpectedConditions.visibilityOfElementLocated(loginCloseBtn));
+		login_closebtn.click();
 		
-		Thread.sleep(5000);
-		driver.findElement(By.xpath("//a[@class='close']")).click();
-		
+	}
+	
+	@Test(priority=3)
+	public void flightsTab() {
+		WebElement flight_tab=wait.until(ExpectedConditions.visibilityOfElementLocated(flightTab));
+		flight_tab.click();
+	}
+	
+	@Test(priority=4)
+	public void selectTripType() {
+		WebElement one_way=wait.until(ExpectedConditions.visibilityOfElementLocated(oneWay));
+		one_way.click();
 	}
 
 }
